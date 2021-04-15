43fb9724f86da0bf41294ad1a1d98f82c54f428c
Hi Dude 123 <59114445+HiDude123@users.noreply.github.com>
2021-04-15 16:09:40 +0200

    Update Server-Start.sh

--- GMOD Linux Scripts/Server-Start.sh
+++ GMOD Linux Scripts/Server-Start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting up Garry's Mod TTT Server."
-screen -AmdS TTTServer ./srcds_run -console -game garrysmod +map ttt_minecraft_b5 +maxplayers 8 +gamemode terrortown +host_workshop_collection 2300187741
+screen -AmdS TTTServer ./srcds_run -console -game garrysmod +map ttt_minecraft_b5 +maxplayers 8 +gamemode terrortown +host_workshop_collection xxxx
 echo "Started. Type (screen -r) to resume!"
